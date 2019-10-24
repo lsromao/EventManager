1b6fd878bf06c9f5f8ed854e4d433d4868dd9918
Lucas Romao <lucas.souzaromao@gmail.com>
2019-10-24 22:18:20 +0200

    MISC-001 - Added Layout for login page and example classes to login

---
+++ app/src/main/java/com/uni/lu/eventmanager/login/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.uni.lu.eventmanager.login;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+import com.uni.lu.eventmanager.R;
+
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //for changing status bar icon colors
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
+        setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginClick(View View){
+        startActivity(new Intent(this,RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+
+    }
+}
