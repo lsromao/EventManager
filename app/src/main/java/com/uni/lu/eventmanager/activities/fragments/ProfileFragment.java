3302f948170aed9c3f905c23f9a9f93d0e95940f
Lucas Romao <lucas.souzaromao@gmail.com>
2019-11-01 22:50:49 +0100

    MISC-003 - Profile created

--- app/src/main/java/com/uni/lu/eventmanager/activities/fragments/ProfileFragment.java
+++ app/src/main/java/com/uni/lu/eventmanager/activities/fragments/ProfileFragment.java
@@ -4,33 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.squareup.picasso.Picasso;
 import com.uni.lu.eventmanager.R;
+import com.uni.lu.eventmanager.media.CircleTransform;
+import com.uni.lu.eventmanager.model.UserModel;
 import com.uni.lu.eventmanager.view.ProfileView;
 
 public class ProfileFragment extends Fragment {
 
 	private ProfileView profileView;
+	private UserModel userModel;
 
+	//TODO Add user logic in a single class of view and add a better layout
 	public View onCreateView(@NonNull LayoutInflater inflater,
 	                         ViewGroup container, Bundle savedInstanceState) {
-		profileView =
-				ViewModelProviders.of(this).get(ProfileView.class);
-		View           root     = inflater.inflate(R.layout.fragment_profile, container, false);
-		final TextView textView = root.findViewById(R.id.text_slideshow);
-		profileView.getText().observe(this, new Observer<String>() {
-			@Override
-			public void onChanged(@Nullable String s) {
-				textView.setText(s);
-			}
-		});
+
+		View root = inflater.inflate(R.layout.fragment_profile, container, false);
+		 userModel = new UserModel();
+
+		ImageView profile =root.findViewById(R.id.profilePicture);
+		Picasso.get()
+				.load(userModel.getPhotoUrl())
+				.resize(450, 450)
+				.centerCrop()
+				.transform(new CircleTransform())
+				.into(profile);
+
+		TextView name = root.findViewById(R.id.profileUserName);
+		name.setText(userModel.getUserName());
+
+		TextView mobile = root.findViewById(R.id.profileMobile);
+		mobile.setText("3333");
+
+		TextView email = root.findViewById(R.id.profileEmail);
+		email.setText(userModel.getEmail());
+
+		TextView address = root.findViewById(R.id.profileAddress);
+		address.setText("Uni LU");
+
+		TextView date = root.findViewById(R.id.profileDate);
+		date.setText("27/12/98");
+
 		return root;
 	}
 }
--- app/src/main/java/com/uni/lu/eventmanager/view/ProfileView.java
+++ app/src/main/java/com/uni/lu/eventmanager/view/ProfileView.java
@@ -1,19 +1,7 @@
 package com.uni.lu.eventmanager.view;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class ProfileView extends ViewModel {
 
-	private MutableLiveData<String> mText;
-
-	public ProfileView() {
-		mText = new MutableLiveData<>();
-		mText.setValue("Profile");
-	}
-
-	public LiveData<String> getText() {
-		return mText;
-	}
 }
